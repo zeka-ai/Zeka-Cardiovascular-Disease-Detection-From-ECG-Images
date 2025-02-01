a42297e963d0dd4622c34f39669f6535463455bd
zeka-ai <zekaaitech@gmail.com>
2025-02-01 23:51:25 +0330

    Update Ecg.py

--- Ecg.py
+++ Ecg.py
@@ -223,7 +223,7 @@ class ECG:
 		returns the final dataframe
 		"""
 		#first load the trained pca
-		pca_loaded_model = joblib.load('\PCA_ECG.pkl')
+		pca_loaded_model = joblib.load('PCA_ECG.pkl')
 		result = pca_loaded_model.transform(test_final)
 		final_df = pd.DataFrame(result)
 		return final_df
@@ -233,7 +233,7 @@ class ECG:
 		This Function Loads the pretrained model and perfrom ECG classification
 		return the classification Type.
 		"""
-		loaded_model = joblib.load('\Heart_Disease_Prediction_using_ECG.pkl')
+		loaded_model = joblib.load('Heart_Disease_Prediction_using_ECG.pkl')
 		result = loaded_model.predict(final_df)
 		if result[0] == 1:
 			return "You ECG corresponds to Myocardial Infarction"
